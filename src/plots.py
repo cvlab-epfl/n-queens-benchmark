eadc22b937c027ee38bcadf1f7671c888028c322
Krzysztof Lis <adynathos@gmail.com>
2020-01-16 20:59:57 +0100

    plots: + java + scala

--- src/plots.py
+++ src/plots.py
@@ -21,7 +21,9 @@ def draw_benchmarks_plots(out_path, rows):
 	plot.set_xlabel('number of queens')
 	plot.set_ylabel('time [s]')
 	fig.tight_layout()
-
+	
+	out_path.parent.mkdir(parents=True, exist_ok=True)
+	
 	# Save plot
 	for ext in FORMATS:
 		fig.savefig(out_path.with_suffix(ext))
@@ -30,37 +32,41 @@ def draw_benchmarks_plots(out_path, rows):
 	plot.set_yscale('log')
 
 	out_path_log = out_path.parent / f'{out_path.stem}_log'
+	
 	for ext in FORMATS:
 		fig.savefig(out_path_log.with_suffix(ext))
 
 
-draw_benchmarks_plots('../data/plots/sequential', [
-	Row('Python', '../data/python_pure.csv', 'r'),
-	Row('Numba', '../data/python_numba_seq.csv', 'g'),
-	Row('C++ stack', '../data/cplus_at_stack.csv', 'm'),
-	Row('C++ heap', '../data/cplus_at_new.csv', 'b'),
-])
+# draw_benchmarks_plots('../data/plots/sequential', [
+# 	Row('Python', '../data/python_pure.csv', 'r'),
+# 	Row('Numba', '../data/python_numba_seq.csv', 'g'),
+# 	Row('C++ stack', '../data/cplus_at_stack.csv', 'm'),
+# 	Row('C++ heap', '../data/cplus_at_new.csv', 'b'),
+# ])
 
-draw_benchmarks_plots('../data/plots/parallel', [
-	Row('Numba Para', '../data/python_numba_para.csv', 'r'),
-	Row('Numba Pool', '../data/python_numba_pool.csv', 'b'),
-	Row('C++ stack', '../data/cplus_para_stack.csv', 'm'),
-	Row('C++ heap', '../data/cplus_para_new.csv', '#ff00ff'),
-])
+# draw_benchmarks_plots('../data/plots/parallel', [
+# 	Row('Numba Para', '../data/python_numba_para.csv', 'r'),
+# 	Row('Numba Pool', '../data/python_numba_pool.csv', 'b'),
+# 	Row('C++ stack', '../data/cplus_para_stack.csv', 'm'),
+# 	Row('C++ heap', '../data/cplus_para_new.csv', '#ff00ff'),
+# ])
 
-draw_benchmarks_plots('../data/plots/sequential_cpp_array_types', [
-	Row('vector<uint8_t>', '../data/cplus_at_vbyte.csv', 'r'),
-	Row('vector<bool>', '../data/cplus_at_vbool.csv', 'b'),
-	Row('new bool[n]', '../data/cplus_at_new.csv', 'g'),
-	Row('array<bool, 32>', '../data/cplus_at_stack.csv', 'm'),
-])
+# draw_benchmarks_plots('../data/plots/sequential_cpp_array_types', [
+# 	Row('vector<uint8_t>', '../data/cplus_at_vbyte.csv', 'r'),
+# 	Row('vector<bool>', '../data/cplus_at_vbool.csv', 'b'),
+# 	Row('new bool[n]', '../data/cplus_at_new.csv', 'g'),
+# 	Row('array<bool, 32>', '../data/cplus_at_stack.csv', 'm'),
+# ])
 
 draw_benchmarks_plots('../data/plots/sequential_with_julia', [
 	Row('Python', '../data/python_pure.csv', 'r'),
-	Row('Numba', '../data/python_numba_seq.csv', 'g'),
-	Row('C++ stack', '../data/cplus_at_stack.csv', 'm'),
-	Row('C++ heap', '../data/cplus_at_new.csv', 'b'),
-	Row('Julia', '../data/julia_seq.csv', 'k'),
+	Row('Numba', '../data/python_numba_seq.csv', '#820002'),
+	Row('C++ stack', '../data/cplus_seq_stack_lambda.csv', 'k'),
+	Row('C++ heap', '../data/cplus_at_new.csv', '#828282'),
+	Row('Julia', '../data/julia_seq.csv', 'c'),
+	Row('Scala rec', '../data/scala_seq_recursive.csv', '#82005b'),
+	Row('Scala closure', '../data/scala_seq_closure.csv', '#d60052'),
+	Row('Java', '../data/java_seq_class.csv', '#d6711f'),
 ])
 
 draw_benchmarks_plots('../data/plots/parallel_with_julia', [
